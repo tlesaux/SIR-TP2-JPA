9e6af562d092d214351f071662dd611593ee7db0
Julien Perrier <perrierjulien22@gmail.com>
2023-03-25 11:07:58 +0100

    servlet

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -27,14 +27,14 @@ public class JpaTest {
         EntityManager manager = factory.createEntityManager();
         JpaTest test = new JpaTest(manager);
 
-        EntityTransaction tx = manager.getTransaction();
-        tx.begin();
+//        EntityTransaction tx = manager.getTransaction();
+//        tx.begin();
         try {
             test.fillDatabase();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        tx.commit();
+       // tx.commit();
 
         test.listTickets();
 
@@ -57,9 +57,14 @@ public class JpaTest {
             User assigneeJulien = new User("Julien");
             User assigneeJohan = new User("Johan");
 
-            manager.persist(reporterTristan);
+            UserDao userDao = new UserDao();
+            userDao.save(reporterTristan);
+            userDao.save(assigneeJulien);
+            userDao.save(assigneeJohan);
+
+            /*manager.persist(reporterTristan);
             manager.persist(assigneeJulien);
-            manager.persist(assigneeJohan);
+            manager.persist(assigneeJohan);*/
 
             Ticket ticket1 = new Ticket("Ajout dashboard", "Ajout d'un dashboard sur l'écran d'accueil", reporterTristan);
             ticket1.setAssignee(assigneeJulien);
@@ -71,9 +76,14 @@ public class JpaTest {
             ticket1.setAssignee(assigneeJulien);
             ticket1.addTag(Tag.TECH);
 
-            manager.persist(ticket1);
+            TicketDao ticketDao = new TicketDao();
+            ticketDao.save(ticket1);
+            ticketDao.save(ticket2);
+            ticketDao.save(ticket3);
+
+         /*   manager.persist(ticket1);
             manager.persist(ticket2);
-            manager.persist(ticket3);
+            manager.persist(ticket3);*/
 
             Message message1 = new Message(assigneeJohan, ticket2, "L'erreur vient-elle du back ou du front ?");
             Message message2 = new Message(reporterTristan, ticket2, "Ca peut-être les deux, a vous de trouver");
@@ -82,12 +92,20 @@ public class JpaTest {
             Message message5 = new Message(reporterTristan, ticket3, "Non, il faut passer les versions une par une");
             Message message6 = new Message(assigneeJulien, ticket3, "Ca marche !");
 
-            manager.persist(message1);
+            MessageDao messageDao = new MessageDao();
+            messageDao.save(message1);
+            messageDao.save(message2);
+            messageDao.save(message3);
+            messageDao.save(message4);
+            messageDao.save(message5);
+            messageDao.save(message6);
+
+           /* manager.persist(message1);
             manager.persist(message2);
             manager.persist(message3);
             manager.persist(message4);
             manager.persist(message5);
-            manager.persist(message6);
+            manager.persist(message6);*/
 
 
           /*  UserDao userDao = new UserDao();
--- src/main/java/servlet/TicketInfo.java
+++ src/main/java/servlet/TicketInfo.java
@@ -1,34 +1,36 @@
 package servlet;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
+
+import dao.TicketDao;
+import jpa.business.Ticket;
+import jpa.business.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
-@WebServlet(name="ticketinfo",
-        urlPatterns={"/TicketInfo"})
+
+
+@WebServlet(name = "ticketinfo", urlPatterns = {"/TicketInfo"})
 public class TicketInfo extends HttpServlet {
-    public void doPost(HttpServletRequest request,
-                       HttpServletResponse response)
-            throws ServletException, IOException {
+    TicketDao ticketDao;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        this.ticketDao = new TicketDao();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-
         PrintWriter out = response.getWriter();
+        Ticket ticket = new Ticket(request.getParameter("name"), "Ma description,",new User("Tristan"));
+        ticketDao.save(ticket);
 
-        out.println("<HTML>\n<BODY>\n" +
-                "<H1>Tickets</H1>\n" +
-                "<UL>\n" +
-                " <LI>Nom: "
-                + request.getParameter("name") + "\n" +
-                " <LI>Prenom: "
-                + request.getParameter("firstname") + "\n" +
-                " <LI>Age: "
-                + request.getParameter("age") + "\n" +
-                "</UL>\n" +
-                "</BODY></HTML>");
+       out.println("<HTML>\n<BODY>\n" + "<H1>Tickets</H1>\n" + "<UL>\n" + " <LI>Nom: " + request.getParameter("name") + "\n" + " <LI>Prenom: " + request.getParameter("firstname") + "\n" + " <LI>Age: " + request.getParameter("age") + "\n" + "</UL>\n" + "</BODY></HTML>");
     }
 }
